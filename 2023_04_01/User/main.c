6c35fee7e7b90568664bc009dc09f4270594f274
ZaxonChow <saxon12@126.com>
2023-07-13 17:41:19 +0800

    modify

--- 2023_04_01/User/main.c
+++ 2023_04_01/User/main.c
@@ -2,17 +2,6 @@
 
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_SetBits(GPIOC, GPIO_Pin_13);
-//	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-//	RCC->APB2ENR = 0X00000010;
-//	GPIOC->CRH = 0X00300000;
-//	GPIOC->ODR = 0X00002000;
 	while(1)
 	{
 		;
